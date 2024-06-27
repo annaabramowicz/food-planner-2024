a3f73c3c1ec65445fa48fe67321039102cb21c28
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2024-06-27 19:18:11 +0200

    [FA-6] Configuration project

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
 import store from "store/store";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
@@ -8,13 +7,11 @@ import { RouterProvider } from "react-router-dom";
 import router from "routes/router";
 import theme from "app/style/theme/theme";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <Provider store={store}>
-        <RouterProvider router={router}>
-          <App />
-        </RouterProvider>
+        <RouterProvider router={router}/>
       </Provider>
     </ChakraProvider>
   </React.StrictMode>
