fc2f9fc5668c09284da200dea6bbc0d304f53faf
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2024-07-05 12:39:07 +0200

    Fa 8 create react app to vite migration (#11)

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: import.meta.env.VITE_APP_API_KEY,
   apiCdnUrl: `https://spoonacular.com/cdn/`,
   apiUrl: `https://api.spoonacular.com/`,
 };
