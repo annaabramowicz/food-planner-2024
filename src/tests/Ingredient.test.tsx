55de357803bb1df65d510617cc271c4302dcfe36
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2024-08-28 18:38:10 +0200

    [FA-10] Ingredient test complete

--- src/tests/Ingredient.test.tsx
+++ src/tests/Ingredient.test.tsx
@@ -2,78 +2,69 @@ import { render, screen } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import Ingredient from "app/pages/IngredientsPage/IngredientsList/Ingredient/Ingredient";
 import { useFridgeData } from "store/fridge/fridge";
+import { useAppDispatch } from "store/useAppDispatch";
+
 import { describe, expect, it, vi } from "vitest";
-import { useAppDispatch } from "store/store";
 
-vi.mock("store/store", () => {
-  return { useAppDispatch: vi.fn() };
-});
+const useFridgeDataMock = vi.mocked(useFridgeData);
+const useAppDispatchMock = vi.mocked(useAppDispatch);
 
-vi.mock("store/fridge/fridge", () => {
-  return {
-    useFridgeData: vi.fn(),
-    saveIngredientToFridgeAsync: vi.fn(),
-    removeIngredientFromFridgeAsync: vi.fn(),
-  };
-});
+vi.mock("store/useAppDispatch");
+vi.mock("store/fridge/fridge");
 
 describe("Ingredient", () => {
-  // it("show ingredient name", () => {
-  //   vi.mock("store/fridge/fridge", () => {
-  //     return { useFridgeData: vi.fn().mockReturnValue({ ingredients: [] }) };
-  //   });
-  //   const ingredient = { id: 9, name: "apple", image: "apple.png" };
-  //   render(<Ingredient ingredient={ingredient} />);
-  //   expect(screen.getByText(/apple/i)).toBeInTheDocument();
-  // });
+  it("show ingredient name", () => {
+    useFridgeDataMock.mockReturnValue({ ingredients: [] });
+    const ingredient = { id: 9, name: "apple", image: "apple.png" };
 
-  // it("show ingredient image", () => {
-  //   vi.mock("store/fridge/fridge", () => {
-  //     return { useFridgeData: vi.fn().mockReturnValue({ ingredients: [] }) };
-  //   });
-  //   const ingredient = { id: 9, name: "apple", image: "apple.png" };
-  //   render(<Ingredient ingredient={ingredient} />);
-  //   const ingredientImg = screen.getByRole("img");
-  //   expect(ingredientImg).toBeInTheDocument();
-  //   expect(ingredientImg).toHaveAttribute(
-  //     "src",
-  //     "https://spoonacular.com/cdn/ingredients_100x100/apple.png"
-  //   );
-  //   expect(ingredientImg).toHaveAttribute("alt", "apple");
-  // });
+    render(<Ingredient ingredient={ingredient} />);
 
-  // it("shows checkmark if ingredient is present in user's fridge", () => {
-  //   vi.mock("store/fridge/fridge", () => {
-  //     return {
-  //       useFridgeData: vi.fn().mockReturnValue({
-  //         ingredients: [{ id: 9, name: "apple", image: "apple.png" }],
-  //       }),
-  //     };
-  //   });
-  //   render(
-  //     <Ingredient ingredient={{ id: 9, name: "apple", image: "apple.png" }} />
-  //   );
-  //   screen.getByRole("checkmark");
-  // });
-  // it("does not shows checkmark if ingredient is not present in user's fridge", () => {
-  //   vi.mock("store/fridge/fridge", () => {
-  //     return {
-  //       useFridgeData: vi.fn().mockReturnValue({
-  //         ingredients: [{ id: 9, name: "apple", image: "apple.png" }],
-  //       }),
-  //     };
-  //   });
-  //   render(
-  //     <Ingredient ingredient={{ id: 7, name: "lemon", image: "lemon.png" }} />
-  //   );
-  //   const checkmark = screen.queryByRole("checkmark");
-  //   expect(checkmark).not.toBeInTheDocument();
-  // });
+    expect(screen.getByText(/apple/i)).toBeInTheDocument();
+  });
+
+  it("show ingredient image", () => {
+    useFridgeDataMock.mockReturnValue({ ingredients: [] });
+    const ingredient = { id: 9, name: "apple", image: "apple.png" };
+
+    render(<Ingredient ingredient={ingredient} />);
+    const ingredientImg = screen.getByRole("img");
+
+    expect(ingredientImg).toBeInTheDocument();
+    expect(ingredientImg).toHaveAttribute(
+      "src",
+      "https://spoonacular.com/cdn/ingredients_100x100/apple.png"
+    );
+    expect(ingredientImg).toHaveAttribute("alt", "apple");
+  });
+
+  it("shows checkmark if ingredient is present in user's fridge", () => {
+    useFridgeDataMock.mockReturnValue({
+      ingredients: [{ id: 9, name: "apple", image: "apple.png" }],
+    });
+
+    render(
+      <Ingredient ingredient={{ id: 9, name: "apple", image: "apple.png" }} />
+    );
+    screen.getByRole("checkmark");
+  });
+
+  it("does not shows checkmark if ingredient is not present in user's fridge", () => {
+    useFridgeDataMock.mockReturnValue({
+      ingredients: [{ id: 9, name: "apple", image: "apple.png" }],
+    });
+
+    render(
+      <Ingredient ingredient={{ id: 7, name: "lemon", image: "lemon.png" }} />
+    );
+    const checkmark = screen.queryByRole("checkmark");
+
+    expect(checkmark).not.toBeInTheDocument();
+  });
 
   it("show checkmark after click ingredient", async () => {
     user.setup();
-    useAppDispatch.mockReturnValue(vi.fn());
-    useFridgeData.mockReturnValue({
+    useAppDispatchMock.mockReturnValue(vi.fn());
+    useFridgeDataMock.mockReturnValue({
       ingredients: [{ id: 8, name: "onion", image: "onion.png" }],
     });
     const { rerender } = render(
@@ -83,7 +74,7 @@ describe("Ingredient", () => {
     const image = screen.getByRole("img", { name: "apple" });
     await user.click(image);
 
-    useFridgeData.mockReturnValue({
+    useFridgeDataMock.mockReturnValue({
       ingredients: [
         { id: 8, name: "onion", image: "onion.png" },
         { id: 9, name: "apple", image: "apple.png" },
@@ -99,8 +90,8 @@ describe("Ingredient", () => {
 
   it("remove checkmark after click ingredient", async () => {
     user.setup();
-    useAppDispatch.mockReturnValue(vi.fn());
-    useFridgeData.mockReturnValue({
+    useAppDispatchMock.mockReturnValue(vi.fn());
+    useFridgeDataMock.mockReturnValue({
       ingredients: [
         { id: 8, name: "onion", image: "onion.png" },
         { id: 9, name: "apple", image: "apple.png" },
@@ -113,7 +104,7 @@ describe("Ingredient", () => {
 
     const image = screen.getByRole("img", { name: "apple" });
     await user.click(image);
-    useFridgeData.mockReturnValue({
+    useFridgeDataMock.mockReturnValue({
       ingredients: [{ id: 8, name: "onion", image: "onion.png" }],
     });
 
