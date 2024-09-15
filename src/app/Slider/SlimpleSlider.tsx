8ad0287fcf78a5c9e368071fb1656591356a54bd
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2024-09-15 11:10:09 +0200

    [FA-11] debug

--- src/app/Slider/SlimpleSlider.tsx
+++ src/app/Slider/SlimpleSlider.tsx
@@ -1,21 +1,30 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Heading from "components/Heading/Heading";
+import initialIngredients from "store/ingredients/initialIngredients";
 
 export default function SimpleSlider() {
+  const apiCdnUrl = `https://spoonacular.com/cdn/`;
+  const imageSize = `100x100`;
+  const imageUrl = `${apiCdnUrl}ingredients_${imageSize}/`;
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 4,
+    speed: 3000,
+    slidesToShow: 3,
     slidesToScroll: 1,
+    // autoplay: true,
+    variableWidth: true,
   };
   return (
     <Slider {...settings}>
-      <Heading>Ania</Heading>
-      <Heading>Tomek</Heading>
-      <Heading>Ala</Heading>
+      {initialIngredients.map((ingredient) => (
+        <img
+          height="100px"
+          width="100px"
+          src={`${imageUrl}${ingredient.image}`}
+        />
+      ))}
     </Slider>
   );
 }
