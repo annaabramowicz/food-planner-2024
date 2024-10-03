7325e5de83c7964481e39435fe631de276ac80e6
Anna Abramowicz <42539465+annaabramowicz@users.noreply.github.com>
2024-10-03 14:16:01 +0200

    [FA-10] test Show ingredient from localStorage in Fridge Page

--- src/tests/IntegrationTests/ShowIngredientFromLocalStorageInFridge.test.tsx
+++ src/tests/IntegrationTests/ShowIngredientFromLocalStorageInFridge.test.tsx
@@ -1,32 +1,25 @@
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import { logRoles, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import store from "store/store";
 import { beforeEach, describe, it } from "vitest";
 import router from "routes/router";
-import {
-  getIngredientsFromLocalStorage,
-  saveIngredientInLocalStorage,
-} from "services/localStorage";
+import { saveIngredientInLocalStorage } from "services/localStorage";
 
 beforeEach(() => {
-  const ingredient = {
+  const plumIngredient = {
     id: 9279,
     name: "purple plum",
     image: "plum.jpg",
   };
-  // window.localStorage.setItem("fridge", JSON.stringify(ingredient));
-  // localStorage.setItem("fridge", JSON.stringify(ingredient));
-  saveIngredientInLocalStorage(ingredient);
-  getIngredientsFromLocalStorage();
+  saveIngredientInLocalStorage(plumIngredient);
 });
 
 describe("Show ingredient from localStorage in Fridge Page", () => {
   it("If user have saved ingredient in localStorage, show them in Fridge Page", async () => {
-    // setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
-    const view = render(
+    render(
       <ChakraProvider theme={theme}>
         <Provider store={store}>
           <RouterProvider router={router} />
@@ -38,10 +31,8 @@ describe("Show ingredient from localStorage in Fridge Page", () => {
     });
 
     await userEvent.click(fridgeLinkPage);
-    logRoles(view.container);
-
-    // screen.getByRole("img", {
-    //   name: "avocados",
-    // });
+    screen.getByRole("img", {
+      name: "purple plum",
+    });
   });
 });
